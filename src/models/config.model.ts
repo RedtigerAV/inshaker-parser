e2a0b4196e5f48817d8cf9c4373e4ac61f794c3e
Anton Vakhrushin <avvakhrushin@gmail.com>
2025-05-24 06:10:51 +0300

    TASK-8: Implemented filters parser (#3)

--- src/models/config.model.ts
+++ src/models/config.model.ts
@@ -2,6 +2,7 @@ export interface Config {
     url: string;
     pages: number;
     outputDir: string;
+    parsedFilters: Array<{ name: string; path: string, outputFile: string }>;
     parsedCocktailsFile: string;
     parsedIngredientsFile: string;
 }
